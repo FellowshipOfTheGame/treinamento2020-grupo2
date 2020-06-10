50b954fba87735fa1d6718d1e92964f0f5630926
Sfrev <51010839+Sfrev@users.noreply.github.com>
2020-06-10 19:37:10 -0300

    Primary character movement

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    [SerializeField] Transform characterTransform = null;
+
+    private Vector3 velocity = Vector3.zero;
+    public float smoothTime = 0.1f;
+
+    Vector3 offset = new Vector3(0, 0, -10);
+
+    void Update()
+    {
+        Vector3 desiredPosition = characterTransform.position + offset;
+        Vector3 smoothPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothTime);
+
+        transform.position = smoothPosition;
+    }
+}
